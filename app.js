fd4f5e49372e82b84bc14acf9bf77268c46e1657
nexseedcamille <ykamimori@nexseed.net>
2019-08-28 12:55:29 +0900

    28の授業分
    関数、タイマー処理、配列途中まで練習。

--- app.js
+++ app.js
@@ -130,3 +130,181 @@ if (number % 5 === 0) {
 } else {
   console.log(number);
 }
+
+console.log('----------------繰り返し処理----------------');
+console.log('for----------------');
+// for( 変数の定義, 条件式, 変数の更新)
+// i
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+console.log('while----------------');
+let j = 11;
+while (j <= 20) {
+  console.log(j);
+  j += 2;
+}
+
+console.log('----------------アラート----------------');
+// alert('こんにちは世界');
+// const answer = confirm('削除しますか?');
+// console.log(answer);
+
+// const value = prompt('あなたの年齢は幾つですか？', '不明');
+// console.log(`${value}です`);
+
+
+// for文
+
+// 1. 100 ~ 1 までconsole.logで出力
+// 2. 3の倍数でBuzzと出力。それ以外は数字を出力
+// 3. if else を使って5の倍数で Fizzを出力する条件を足してください。
+// 4. 5 と 3の倍数の時に Fizz Buzz と出力する.
+
+// for (let i = 100; i > 0; i--) {
+//   if (i % 3 == 0 && i % 5 == 0) {
+//     console.log('Fizz Buzz');
+//   } else if (i % 3 == 0) {
+//     console.log('Buzz');
+//   } else if (i % 5 == 0) {
+//     console.log('Fizz');
+//   } else {
+//     console.log(i);
+//   }
+// }
+
+console.log('----------------関数----------------');
+
+/*
+    自作関数
+    複数の処理を1つにまとめたもの
+    function 関数名() {
+        処理
+    }
+    組込関数
+    alert()
+*/
+// function greeting() {
+//   console.log('おはよう');
+// }
+
+// greeting();
+
+console.log('引数----------------');
+
+// // 関数の中で使える変数を引数と呼びます
+// function greeting(greeting) {
+//   console.log(greeting);
+// }
+
+// greeting('こんにちは')
+// greeting('こんばんは')
+
+console.log('ローカル変数----------------');
+// function greeting(name) {
+//   let morning = 'おはよう' + name;
+//   // 値を取り出すために必要
+//   // console.log()
+//   // 値を取り出すための処理が走っているからreturn;
+//   return morning;
+// }
+
+// let hoge = greeting('ヒロ');
+// console.log(hoge)
+
+console.log('関数を変数に代入----------------');
+// let greeting = function (name) {
+//   let greeting = 'おはよう' + name;
+//   return greeting;
+// }
+
+// let hoge = greeting('seedKun');
+// console.log(hoge);
+
+// function addtax(product, num, taxrate) {
+//   return product * num * taxrate;
+// }
+// // 醜くなるコードを整理するために使うことあります
+// let result = addtax(500, 10, 1.1);
+// console.log(result);
+
+// // 勝った数 / 全試合数 * 100
+// function winrate(teamname, win, all) {
+//   return teamname + 'の勝率は' + win / all * 100 + '%です。'
+// }
+
+// // 福岡ソフトバンク 120 61
+// let result = winrate('カープ', 61, 120);
+// console.log(result);
+
+// 一日何時間したら 何日呉に2000時間に到達できるか
+function days(time) {
+  let result = 2000 / time
+  return result;
+}
+let result = days(3);
+console.log(result);
+
+console.log('----------------タイマー処理----------------');
+/*
+    タイマー処理
+    setInterval ->前の処理が終わったことを確認しない
+    setTimeout ->前の処理が終わったか確認
+*/
+
+console.log('setInterval----------------');
+// let i = 100;
+
+// function countDown() {
+//   if (i >= 95) {
+//     console.log(i--);
+//   }
+// }
+
+// setInterval(function () {
+//   countDown();
+// }, 1000);
+
+console.log('setTimeout----------------');
+// let j2 = 100;
+// function countDown() {
+//   console.log(j2--);
+// }
+
+// setTimeout(function () {
+//   countDown();
+// }, 1000);
+
+console.log('setTimeOutで繰り返し + timerをとめる----------------');
+// let k = 100;
+// function countDown() {
+//   console.log(k--);
+//   let timer = setTimeout(function () {
+//     countDown();
+//   }, 1000);
+
+//   if (k < 95) {
+//     clearTimeout(timer)
+//   }
+// }
+// countDown();
+
+console.log('----------------配列----------------');
+// 配列 = グループ化されたデータ
+// [値1, 値2]
+let student_list = ['Yamada', 'Sato', 'Suzuki'];
+console.log(student_list);
+console.log(student_list[2]);
+console.log(student_list[2] = 'KOBAYASI');
+console.log(student_list);
+// チェーンメソッド
+student_list.push('Takahashi');
+console.log(student_list);
+student_list.pop();
+console.log(student_list);
+student_list.shift();
+console.log(student_list);
+
+// 関数化
+// ネットフィリックスとアマゾンプライムの合計
